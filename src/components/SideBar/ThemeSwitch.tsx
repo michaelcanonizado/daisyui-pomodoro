621a7de09e11ea1fa05d05c3792c68e1f53d35c6
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-12-19 21:52:50 +0800

    adjusted theme switcher alignment

--- src/components/SideBar/ThemeSwitch.tsx
+++ src/components/SideBar/ThemeSwitch.tsx
@@ -9,11 +9,11 @@ const ThemeSwitch: FC<IThemeSwitch> = (props): ReactElement => {
 	const { icon, text } = props;
 
 	return (
-		<button className="py-2 pr-4 pl-16 flex lg:mt-auto hover:bg-base-200 transition-colors ease-linear">
+		<button className="py-2 pr-4 pl-20 flex items-center lg:mt-auto hover:bg-base-200 transition-colors ease-linear">
 			<span className="mr-5">
 				<img className="w-8" src={icon} alt={text} />
 			</span>
-			<div className="flex items-center">
+			<div className="">
 				<span className="text-base">{text}</span>
 			</div>
 		</button>
