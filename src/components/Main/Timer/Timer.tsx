8dbac83ac03aef4ffd8c3d7bd23de3d48568bf12
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-12-19 23:30:40 +0800

    added timer buttons

--- src/components/Main/Timer/Timer.tsx
+++ src/components/Main/Timer/Timer.tsx
@@ -6,20 +6,20 @@ import SkipIcon from './SkipIcon';
 
 const Timer: FC = (): ReactElement => {
 	return (
-		<main>
-			<div className="">
+		<main className="grow shrink flex flex-col">
+			<div className="h-[50px]">
 				<span className="">Sign In</span>
 			</div>
-			<section className="">
+			<section className="m-auto">
 				<div className=""></div>
-				<div className="">
-					<button className="btn btn-circle btn-outline btn-sm">
+				<div className="flex items-center gap-6">
+					<button className="btn btn-circle btn-sm p-1 border border-neutral">
 						<RestartIcon />
 					</button>
-					<button className="btn btn-circle btn-outline btn-sm">
+					<button className="btn btn-circle p-2 border border-neutral">
 						<PlayIcon />
 					</button>
-					<button className="btn btn-circle btn-outline btn-sm">
+					<button className="btn btn-circle btn-sm p-1 border border-neutral">
 						<SkipIcon />
 					</button>
 				</div>
