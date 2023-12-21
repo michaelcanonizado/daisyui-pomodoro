72e282ee90b1d03963a3669d2dbe0dde468ecbfd
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-12-21 19:24:40 +0800

    refactored svg component

--- src/components/SideBar/DownArrowIcon.tsx
+++ src/components/SideBar/DownArrowIcon.tsx
@@ -3,22 +3,10 @@ import React from 'react';
 const DownArrowIcon = () => {
 	return (
 		<svg width="12" height="12" viewBox="0 0 12 12" fill="currentFill">
-			<g clip-path="url(#clip0_12_2)">
-				<path
-					d="M1.45898 9.95508L0.0410155 8.54297L6 2.58398L11.959 8.54297L10.541 9.95508L6 5.41406L1.45898 9.95508Z"
-					fill="black"
-				/>
-			</g>
-			<defs>
-				<clipPath id="clip0_12_2">
-					<rect
-						width="12"
-						height="12"
-						fill="white"
-						transform="translate(12 12) rotate(180)"
-					/>
-				</clipPath>
-			</defs>
+			<path
+				d="M1.45898 9.95508L0.0410155 8.54297L6 2.58398L11.959 8.54297L10.541 9.95508L6 5.41406L1.45898 9.95508Z"
+				fill="black"
+			/>
 		</svg>
 	);
 };
