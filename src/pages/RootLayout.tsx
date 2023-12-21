c8621cc973b0dc52214048f200c33296ed477b27
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-12-21 17:08:43 +0800

    updated site theme on theme switch

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,15 +1,20 @@
 import React, { FC, ReactElement } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { useAppSelector } from '../app/hooks';
+
 import Sidebar from '../components/Sidebar/Sidebar';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Main/Footer/Footer';
 
 const RootLayout: FC = (): ReactElement => {
+	const theme = useAppSelector((state) => state.theme);
+
 	return (
 		<div
 			className="h-screen flex flex-col lg:flex-row"
 			id="root-layout-breakpoint"
+			data-theme={theme.currentTheme}
 		>
 			<Sidebar className="hidden lg:flex" id="sidebar-height-breakpoint" />
 			<Navbar className="flex lg:hidden" id="navbar-height-breakpoint" />
